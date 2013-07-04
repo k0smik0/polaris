adc8faa54a07e88d7a733534d1a169d79bf0228f
Massimiliano Leone <maximilianus@gmail.com>
2013-07-04 16:43:33 +0200

    added license: lgpl3 to libraries, gpl3 to sample demo app

--- src/utils/net/iubris/polaris/locator/utils/LocationUtils.java
+++ src/utils/net/iubris/polaris/locator/utils/LocationUtils.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
+ * Copyleft 2013 Massimiliano Leone - massimiliano.leone@iubris.net .
  * 
- * Copyleft 2012 Massimiliano Leone - massimiliano.leone@iubris.net .
- * LocationUtils.java is part of 'Diane'.
+ * LocationUtils.java is part of 'Polaris'.
  * 
- * 'Diane' is free software; you can redistribute it and/or modify
+ * 'Polaris' is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
- * 'Diane' is distributed in the hope that it will be useful,
+ * 'Polaris' is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with 'Diane' ; if not, write to the Free Software
+ * along with 'Polaris'; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  ******************************************************************************/
 package net.iubris.polaris.locator.utils;
@@ -86,9 +86,9 @@ Log.d("LocationUtils.isLocationOlder","difference: "+ (System.currentTimeMillis(
 //		if(System.currentTimeMillis() - location.getTime() > timeMinimumThreshold) return true;
 		
 		if (currentLocation == null) {
-	        // A new location is always better than no location
-	        return true;
-	    }
+//			A new location is always better than no location
+			return true;
+		}
 		
 		// es. currentLocation = 180s; newLocation = 10s; timeDelta = 170s
 		final long timeDelta = getTimeDelta(newLocation, currentLocation);		
